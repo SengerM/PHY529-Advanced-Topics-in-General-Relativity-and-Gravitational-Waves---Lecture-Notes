46936e3114b6eb68b2a95479dbe2d03a97b479a5
Matias Senger <m.senger@hotmail.com>
2021-12-16 13:13:06 +0100

    first commit

---
+++ export_to_html.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd  /home/alf/scripts_and_codes/repos/html-academic-publishing/python/
+python3 lyx_to_html.py --lyx-document /home/alf/cernbox/PhD/courses/PHY529\ Advanced\ Topics\ in\ General\ Relativity\ and\ Gravitational\ Waves/my\ stuff/lecture\ notes/PHY529\ lecture\ notes.lyx
